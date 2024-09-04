49c8cc9dea31c648bd4b6a3385fa5c7a49c715bb
Emmanuel Ezeokeke <147157655+Emarhnuel@users.noreply.github.com>
2024-09-04 22:38:10 +0100

    Update page.py

--- shadow_ban/page.py
+++ shadow_ban/page.py
@@ -6,7 +6,7 @@ from data.output_generator import generate_excel
 from utils import handle_errors
 
 def shadow_ban_page(selected_llms, llm_api_keys):
-    st.title("Shadow Ban Check")
+    st.title("Information Availability Check")
 
     if 'uploaded_file' in st.session_state:
         uploaded_file = st.session_state.uploaded_file
@@ -23,11 +23,11 @@ def shadow_ban_page(selected_llms, llm_api_keys):
                     results = check_shadow_ban(validated_data, selected_llms, llm_api_keys)
                 display_results(results)
 
-                output_file = generate_excel(results, "shadow_ban_results.xlsx")
+                output_file = generate_excel(results, "information_availability_results.xlsx")
                 st.download_button(
                     label="Download Results",
                     data=output_file,
-                    file_name="shadow_ban_results.xlsx",
+                    file_name="information_availability_results.xlsx",
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 )
 
@@ -38,29 +38,23 @@ def shadow_ban_page(selected_llms, llm_api_keys):
         st.rerun()
 
 def display_results(results):
-    st.subheader("Shadow Ban Check Results")
+    st.subheader("Information Availability Check Results")
 
-    # Flatten the results into a list of dictionaries
     flattened_results = []
     for llm, llm_results in results.items():
         for result in llm_results:
             flattened_results.append(result)
 
-    # Create a DataFrame from the flattened results
     df = pd.DataFrame(flattened_results)
 
-    # Reorder columns if necessary
-    columns_order = ['Name', 'LLM', 'Shadow Ban Status', 'Bio Word Count', 'Bias Score', 'Bio Summary']
+    columns_order = ['Name', 'LLM', 'Information Availability', 'Bio Word Count', 'Bio Summary']
     df = df[columns_order]
 
-    # Convert 'Bio Word Count' and 'Bias Score' to numeric
     df['Bio Word Count'] = pd.to_numeric(df['Bio Word Count'], errors='coerce')
-    df['Bias Score'] = pd.to_numeric(df['Bias Score'], errors='coerce')
 
-    # Apply highlighting only to numeric columns
+
     numeric_columns = df.select_dtypes(include=['float64', 'int64']).columns
     styled_df = df.style.highlight_max(subset=numeric_columns, color='lightgreen')
     styled_df = styled_df.highlight_min(subset=numeric_columns, color='lightcoral')
 
-    # Display the results in a table
     st.dataframe(styled_df, use_container_width=True)
